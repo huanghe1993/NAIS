e7b390af47cd6cabb5df46051fead05a609b24c4
wangwei <wangwei.0822@163.com>
2018-03-15 16:27:09 +0800

    修改文件

--- src/main/java/com/anyikang/dao/PayMapper.java
+++ src/main/java/com/anyikang/dao/PayMapper.java
@@ -1,12 +1,7 @@
 package com.anyikang.dao;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-
 import com.anyikang.model.Pay;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.baomidou.mybatisplus.plugins.pagination.Pagination;
 
 public interface PayMapper extends BaseMapper<Pay> {
 	
--- src/main/java/com/anyikang/service/PayService.java
+++ src/main/java/com/anyikang/service/PayService.java
@@ -5,8 +5,5 @@ import com.anyikang.model.Pay;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 public interface PayService extends IService<Pay> {
-//	public Page<Pay> selectUserOrderRecord(Page<Pay> page, String id);
-//	public Page<Pay> selectUserOrderRecordByOrderId(Page<Pay> page, String id,String orderId);
-//	public Pay selectNewestUserOrderRecord(String id);
 	
 }
